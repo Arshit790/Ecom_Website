d574bd5e738c65b3c892dfa372af1f0ea2a00fff
Arshit790 <arshitdixit5@gmail.com>
2024-04-13 23:54:24 +0530

    message

--- store/views.py
+++ store/views.py
@@ -56,29 +56,29 @@ def checkout(request):
 
 
 def updateItem(request):
-	data = json.loads(request.body)
-	productId = data['productId']
-	action = data['action']
-	print('Action:', action)
-	print('Product:', productId)
+  data = json.loads(request.body)
+  productId = data['productId']
+  action = data['action']
+  print('Action:', action)
+  print('Product:', productId)
 
-	customer = request.user.customer
-	product = Product.objects.get(id=productId)
-	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+  customer = request.user.customer
+  product = Product.objects.get(id=productId)
+  order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+  orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
-	if action == 'add':
-		orderItem.quantity = (orderItem.quantity + 1)
-	elif action == 'remove':
-		orderItem.quantity = (orderItem.quantity - 1)
+  if action == 'add':
+    orderItem.quantity = (orderItem.quantity + 1)
+  elif action == 'remove':
+    orderItem.quantity = (orderItem.quantity - 1)
 
-	orderItem.save()
+  orderItem.save()
 
-	if orderItem.quantity <= 0:
-		orderItem.delete()
+  if orderItem.quantity <= 0:
+    orderItem.delete()
 
-	return JsonResponse('Item was added', safe=False)
+  return JsonResponse('Item was added', safe=False)
 
 
 
@@ -89,11 +89,12 @@ def processOrder(request):
   if request.user.is_authenticated:
     customer = request.user.customer
     order, created = Order.objects.get_or_create(customer=customer, complete=False)
-    total = float(data['form']['total'])
-    order.transaction_id = transaction_id
+  
+  total = float(data['form']['total'])
+  order.transaction_id = transaction_id
 
-    if total == order.get_cart_total:
-        order.complete = True
+  if total == order.get_cart_total:
+    order.complete = True
     order.save()
 
   else:
@@ -104,12 +105,13 @@ def processOrder(request):
   order.save()
 
   if order.shipping == True:
-	  ShippingAddress.objects.create(
-		customer=customer,
-		order=order,
-		address=data['shipping']['address'],
-		city=data['shipping']['city'],
-		state=data['shipping']['state'],
-		zipcode=data['shipping']['zipcode'],
-		)
-  return JsonResponse('Payment submitted..', safe=False)
+    ShippingAddress.objects.create(
+    customer=customer,
+    order=order,
+    address=data['shipping']['address'],
+    city=data['shipping']['city'],
+    state=data['shipping']['state'],
+    zipcode=data['shipping']['zipcode'],
+    )
+  
+  return JsonResponse('Payment submitted..', safe=False)  
